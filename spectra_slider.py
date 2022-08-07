5d345a9cd095c18feb90e50961e9085be3f328e2
lgolston <30876419+lgolston@users.noreply.github.com>
2022-08-07 20:29:04 +0900

    add HAPI code

--- spectra_slider.py
+++ spectra_slider.py
@@ -18,6 +18,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from datetime import datetime
 from matplotlib.widgets import Slider
+import hapi
+hapi.db_begin('HAPI')
 
 case = 'RF05'
 
@@ -25,6 +27,14 @@ if case == 'RF05':
     filename_f = '../Data/2022-08-06/n2o-co_2022-08-06_f0000.txt'
     filename_s = '../Data/2022-08-06/n2o-co_2022-08-06_s0000.txt'
     cur_day = datetime(2022,8,6)
+
+# %% set up HITRAN Application Programming Interface (HAPI)
+# Download spectral data using the Python library HAPI
+# Can also refer to Los Gatos information used in peaks_000.ini file
+# working in range 4.565-4.567 (2189-2191)
+#hapi.fetch('H2O',1,1,2189,2191)
+#hapi.fetch('CO',5,1,2189,2191)
+#hapi.fetch('N2O',4,1,2189,2191)
 
 # %% load files
 # load spectra file
@@ -80,6 +90,7 @@ class Spectral:
     # key variables
     raw_scan = None
     baseline = None
+    ringdown = None
     
     # spectral parameters
     H2O_center = 340
@@ -87,21 +98,30 @@ class Spectral:
     N2O_center_a = 458
     N2O_center = 542
     CO_center = 806
+    
+    nu_CO,coef_CO = hapi.absorptionCoefficient_Voigt(SourceTables='CO',
+                                                 Diluent={'air':1.0},
+                                                 WavenumberRange = [2189, 2191],
+                                                 WavenumberStep=0.001,
+                                                 Environment={'p':0.1,'T':296})
 
+    nu_N2O,coef_N2O = hapi.absorptionCoefficient_Voigt(SourceTables='N2O',
+                                                 Diluent={'air':1.0},
+                                                 WavenumberRange = [2189, 2191],
+                                                 WavenumberStep=0.001,
+                                                 Environment={'p':0.1,'T':296})
+
+    nu_H2O,coef_H2O = hapi.absorptionCoefficient_Voigt(SourceTables='H2O',
+                                                 Diluent={'air':1.0},
+                                                 WavenumberRange = [2189, 2191],
+                                                 WavenumberStep=0.001,
+                                                 Environment={'p':0.1,'T':296})
+    
     # function called when slider is moved
     def update(self, event):
         # get value of slider
         ii = sfreq.val
-        
-        # select the laser scan
-        x0 = 180+ii*1126
-        x1 = 1123+ii*1126
-        self.raw_scan = np.ravel([float(x) for x in spectra[x0:x1]])
-        
-        # select the ringdown scan
-        #x0 = 17+ii*1126
-        #x1 = 176+ii*1126
-        
+                
         # plot vertical line on concentration time series (top plot)
         l1.set_data([ii,ii],[0,1000])
         
@@ -109,6 +129,16 @@ class Spectral:
         ix = int(ID[ii])
         ax1.set_xlim(ix-50,ix+50)
         ax1.set_ylim(LGR_N2O[ix]-5,LGR_N2O[ix]+5)
+        
+        # select the laser scan
+        x0 = 180+ii*1126
+        x1 = 1123+ii*1126
+        self.raw_scan = np.ravel([float(x) for x in spectra[x0:x1]])
+        
+        # select the ringdown scan
+        x0 = 17+ii*1126
+        x1 = 176+ii*1126
+        self.ringdown = np.ravel([float(x) for x in spectra[x0:x1]])
         
         # calculate spectral baseline
         combined = self.raw_scan[x_fit]
@@ -121,7 +151,11 @@ class Spectral:
         l2.set_ydata(self.baseline-self.raw_scan)
         
         # perform spectral fitting
-        #
+        # ...
+        #plt.plot(nu_CO,coef_CO*100E-9,'b')
+        #plt.plot(nu_N2O,coef_N2O*300E-9,'g')
+        #plt.plot(nu_H2O,coef_H2O*1E-2,'m')
+        #plt.xlim(2189.8,2190.7)
         
         # update figure
         fig.canvas.draw_idle()
