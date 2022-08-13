4cd289cc78f286cbf2ded1ec3383170c40097f20
lgolston <30876419+lgolston@users.noreply.github.com>
2022-08-12 22:21:36 -0700

    intercomparison - add correlation plots

--- COMA_intercomparison.py
+++ COMA_intercomparison.py
@@ -2,10 +2,7 @@
 """
 Compare COLD2, ACOS, and COMA CO measurements
 
-Add:
-- Correlation DLH H2O and CO
-- Intercomparison DLH H2O and COMA H2O
-- Any other N2O measurements?
+Add correlations
 """
 
 # %% header
@@ -16,9 +13,12 @@ from datetime import datetime, timedelta
 import matplotlib.dates as mdates
 from load_flight_functions import read_COMA
 
-case = 'RF05'
+case = 'RF04'
+to_plot = 'CO' # CO, CO-H2O, corr
 
-# %% load data
+# %% list file names
+filename_DLH = None
+
 if case == 'Transit1': # Ellington to Seattle
     filename_ACOS = '../Data/_OtherData_/ACCLIP-ACOS-1Hz_WB57_20220721_RA_1.ict'
     filename_COLD2 = '../Data/_OtherData_/acclip-COLD2-CO_WB57_20220721_RA_L1.ict'
@@ -54,38 +54,141 @@ elif case == 'RF04': # RF04, Osan
     filename_ACOS = '../Data/_OtherData_/ACCLIP-ACOS-1Hz_WB57_20220804_RA.ict'
     filename_COLD2 = '../Data/_OtherData_/acclip-COLD2-CO_WB57_20220804_RA.ict'
     filename_COMA = ['../Data/2022-08-04/n2o-co_2022-08-04_f0000.txt']
+    filename_DLH = '../Data/_OtherData_/ACCLIP-DLH-H2O_WB57_20220804_RA.ict'
     cur_day = datetime(2022,8,4)
 elif case == 'RF05': # RF05, Osan
     filename_ACOS = '../Data/_OtherData_/ACCLIP-ACOS-1Hz_WB57_20220806_RA.ict'
     filename_COLD2 = '../Data/_OtherData_/acclip-COLD2-CO_WB57_20220806_RA.ict'
     filename_COMA = ['../Data/2022-08-06/n2o-co_2022-08-06_f0000.txt']
+    filename_DLH = '../Data/_OtherData_/ACCLIP-DLH-H2O_WB57_20220806_RA.ict'
     cur_day = datetime(2022,8,6)
 
-# %% load and plot data
-fig, ax = plt.subplots(1, 1, figsize=(8,4))
+# %% Plot CO time series from COMA, ACOS, COLD2
+if to_plot == 'CO':
+    fig, ax = plt.subplots(1, 1, figsize=(8,4))
+    
+    # load ACOS
+    if filename_ACOS:
+        ACOS = pd.read_csv(filename_ACOS,sep=',',header=37)
+        ACOS_time = [cur_day+timedelta(seconds=t) for t in ACOS['TIME_START']]
+        plt.plot(ACOS_time,ACOS['ACOS_CO_PPB'],'.m',label='ACOS')
+        
+    # load COLD2
+    if filename_COLD2:
+        COLD2 = pd.read_csv(filename_COLD2,sep=',',header=32)
+        COLD_time = [cur_day+timedelta(seconds=t) for t in COLD2['Time_Start']]
+        plt.plot(COLD_time,COLD2[' CO_COLD2_ppbv'],'.g',label='COLD2')
+        
+    # load COMA
+    if filename_COMA:
+        COMA = read_COMA(filename_COMA)
+        ix_8 = np.ravel(np.where(COMA["      MIU_VALVE"]==8)) # inlet
+        plt.plot(COMA['time'][ix_8],COMA["      [CO]d_ppm"][ix_8]*1000,'b.',label='COMA')
+    
+    ax.set_ylabel('CO, ppb')
+    ax.set_ylim([-10,300])
+    ax.grid('on')
+    ax.legend()
+    ax.set_title(case)
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
+    fig.tight_layout()
+    
+# %% Plot CO time series and DLH H2O
+if to_plot == 'CO-H2O':
+    fig, ax = plt.subplots(1, 1, figsize=(8,4))
+    ax_twin = ax.twinx()
+    
+    # load DLH
+    if filename_DLH:
+        DLH = pd.read_csv(filename_DLH,sep=',',header=35)
+        DLH_time = [cur_day+timedelta(seconds=t) for t in DLH['Time_Start']]
+        ax_twin.plot(DLH_time,DLH['H2O_DLH'],'.k',label='DLH')
+        #ax_twin.plot(DLH_time,DLH['RHw_DLH'],'.k',label='DLH')
+    
+    # load COMA
+    if filename_COMA:
+        COMA = read_COMA(filename_COMA)
+        ix_8 = np.ravel(np.where(COMA["      MIU_VALVE"]==8)) # inlet
+        ax.plot(COMA['time'][ix_8],COMA["      [CO]d_ppm"][ix_8]*1000,'b.',label='COMA')
+        #ax_twin.plot(COMA['time'][ix_8],COMA["      [H2O]_ppm"][ix_8]*1000,'y.',label='COMA-H2O')
 
-# load ACOS
-if filename_ACOS:
-    ACOS = pd.read_csv(filename_ACOS,sep=',',header=37)
-    ACOS_time = [cur_day+timedelta(seconds=t) for t in ACOS['TIME_START']]
-    plt.plot(ACOS_time,ACOS['ACOS_CO_PPB'],'.m',label='ACOS')
+    ax_twin.set_ylabel('DLH water vapor mixing ratio, ppmv') 
+    ax_twin.set_yscale('log')
+
+    ax.set_ylim(0,500)
+    ax.set_ylabel('CO, ppb',color='b')
+    ax.tick_params(axis='y', colors='blue')
+    ax.grid('on')
+    ax.set_title(case)
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
+    fig.tight_layout()
+   
+# %% correlation plots
+# COMA CO vs ACOS
+# COMA CO vs COLD2
+# COMA H2O vs DLH H2O
+
+def sync_ab(df_a,df_b):
+    a_1Hz = df_a.groupby(pd.Grouper(key="time", freq="1s")).mean()
+    b_1Hz = df_b.groupby(pd.Grouper(key="time", freq="1s")).mean()
+    sync_data = pd.merge(a_1Hz, b_1Hz, how='inner', on=['time'])
+    return sync_data
+
+if to_plot == 'corr':
+    fig, ax = plt.subplots(1, 3, figsize=(12,4))
     
-# load COLD2
-if filename_COLD2:
-    COLD2 = pd.read_csv(filename_COLD2,sep=',',header=32)
-    COLD_time = [cur_day+timedelta(seconds=t) for t in COLD2['Time_Start']]
-    plt.plot(COLD_time,COLD2[' CO_COLD2_ppbv'],'.g',label='COLD2')
-    
-# load COMA
-if filename_COMA:
     COMA = read_COMA(filename_COMA)
     ix_8 = np.ravel(np.where(COMA["      MIU_VALVE"]==8)) # inlet
-    plt.plot(COMA['time'][ix_8],COMA["      [CO]d_ppm"][ix_8]*1000,'b.',label='COMA')
+    #COMA['time'][ix_8]
+    #COMA["      [CO]d_ppm"][ix_8]*1000
+    
+    # load ACOS
+    if filename_ACOS:
+        ACOS = pd.read_csv(filename_ACOS,sep=',',header=37)
+        ACOS['time'] = [cur_day+timedelta(seconds=t) for t in ACOS['TIME_START']]
+        
+        df_a = pd.DataFrame({'time': COMA['time'][ix_8], 'CO_COMA': COMA["      [CO]d_ppm"][ix_8]*1000})
+        df_b = pd.DataFrame({'time': ACOS['time'], 'CO_ACOS': ACOS['ACOS_CO_PPB']})
+        sync_data = sync_ab(df_a,df_b)
+        ax[0].plot(sync_data['CO_COMA'],sync_data['CO_ACOS'],'k.')
+    
+    # load COLD2
+    if filename_COLD2:
+        COLD2 = pd.read_csv(filename_COLD2,sep=',',header=32)
+        COLD2['time'] = [cur_day+timedelta(seconds=t) for t in COLD2['Time_Start']]
+        
+        df_a = pd.DataFrame({'time': COMA['time'][ix_8], 'CO_COMA': COMA["      [CO]d_ppm"][ix_8]*1000})
+        df_b = pd.DataFrame({'time': COLD2['time'], 'CO_COLD2': COLD2[' CO_COLD2_ppbv']})
+        sync_data = sync_ab(df_a,df_b)
+        ax[1].plot(sync_data['CO_COMA'],sync_data['CO_COLD2'],'k.')
+    
+    # load DLH
+    if filename_DLH:
+        DLH = pd.read_csv(filename_DLH,sep=',',header=35)
+        DLH['time'] = [cur_day+timedelta(seconds=t) for t in DLH['Time_Start']]
 
-ax.set_ylabel('CO, ppb')
-ax.set_ylim([-10,300])
-ax.grid('on')
-ax.legend()
-ax.set_title(case)
-ax.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
-fig.tight_layout()
+        df_a = pd.DataFrame({'time': COMA['time'][ix_8], 'H2O_COMA': COMA["      [H2O]_ppm"][ix_8]})
+        df_b = pd.DataFrame({'time': DLH['time'], 'H2O_DLH': DLH['H2O_DLH']})
+        sync_data = sync_ab(df_a,df_b)
+        ax[2].plot(sync_data['H2O_COMA'],sync_data['H2O_DLH'],'k.')
+    
+    ax[0].set_xlabel('COMA CO, ppbv')
+    ax[0].set_ylabel('ACOS CO, ppbv')
+    
+    ax[1].set_xlabel('COMA CO, ppbv')
+    ax[1].set_ylabel('COLD2 CO, ppbv')
+    
+    ax[2].set_xlabel('COMA H2O, ppmv')
+    ax[2].set_ylabel('DLH H2O, ppmv')
+    
+    ax[0].plot([0,200],[0,200],'k:')
+    ax[0].set_xlim([0,200])
+    ax[0].set_ylim([0,200])
+    
+    ax[1].plot([0,200],[0,200],'k:')
+    ax[1].set_xlim([0,200])
+    ax[1].set_ylim([0,200])
+    
+    ax[2].plot([0,30000],[0,30000],'k:')
+    
+    fig.tight_layout()
